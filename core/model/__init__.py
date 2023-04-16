bd3feccabca03dbcb82d96bd0706e5c5f12b20ac
DESKTOP-4PIPJNQ\RMC <ivankash888@gmail.com>
2023-04-16 12:51:41 +0500

    New test dataset loaded

--- core/model/__init__.py
+++ core/model/__init__.py
@@ -1,10 +1,11 @@
 import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FasterRCNN_ResNet50_FPN_Weights
 
 
 def build_model(num_classes: int, pretrained: bool = True):
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=pretrained)
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT,
+                                                                 progress=True,
+                                                                 pretrained=pretrained,
+                                                                 trainable_bacbone_layers=5,
+                                                                 num_classes=num_classes)
     return model
-
