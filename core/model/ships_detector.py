ac01ff173db83364150e67fa85957d71fbadaf34
KhutornoyYaroslav <morse1995@mail.ru>
2023-04-18 08:30:58 +0300

    Fix model

--- core/model/ships_detector.py
+++ core/model/ships_detector.py
@@ -5,7 +5,7 @@ from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_
 class ShipsDetector(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+        # self.weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
         # self.model = fasterrcnn_resnet50_fpn_v2(weights=self.weights)
         self.model = fasterrcnn_resnet50_fpn_v2(num_classes=num_classes)
 
