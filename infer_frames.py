0e6b93959a32c41e143447b9c4673b2190a00c4a
KhutornoyYaroslav <morse1995@mail.ru>
2023-04-21 16:53:37 +0300

    Fix infer

--- infer_frames.py
+++ infer_frames.py
@@ -10,14 +10,29 @@ from core.utils.checkpoint import CheckPointer
 from core.data.transforms import ToCV2Image, Resize, ConvertFromInts, Clip, Normalize, ToTensor
 
 
+def resize_box(box, in_size, out_size):
+    width_k = out_size[1] / in_size[1]
+    height_k = out_size[0] / in_size[0]
+
+    box[0] = int(width_k * box[0])
+    box[1] = int(height_k * box[1])
+    box[2] = int(width_k * box[2])
+    box[3] = int(height_k * box[3])
+
+    return box
+
+
 def infer(model, frames, input_size, device):
     torch.cuda.empty_cache()
     model.eval()
 
     for frame_path in tqdm(frames):
-        # Read input frame and transform
-        frame = cv.imread(frame_path)
-        frame, _, _ = Resize(input_size)(frame)
+        # Read input frame
+        frame_cv = cv.imread(frame_path)
+        frame_orig_size = frame_cv.shape[:2]
+
+        # Transforms frame
+        frame, _, _ = Resize(input_size)(frame_cv)
         frame, _, _ = ConvertFromInts()(frame)
         frame, _, _ = Clip()(frame)
         frame, _, _ = Normalize()(frame)
@@ -42,19 +57,20 @@ def infer(model, frames, input_size, device):
                 if scores[i] < PROB_THRESH:
                     continue
 
-                box = boxes[i]
-                cv.rectangle(cv_img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 1)
+                box = resize_box(boxes[i], cv_img.shape[:2], frame_orig_size)
+                cv.rectangle(frame_cv, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (30, 220, 20), 2)
                 label_str = ShipsDataset.CLASSES_INT2STR[labels[i]] + ", {0:.2f}".format(scores[i])
-                cv.putText(cv_img, label_str, (int(box[0]), int(box[1])), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv.putText(frame_cv, label_str, (int(box[0]), int(box[1]) - 5), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
 
-            cv.imshow('Frame', cv_img)
-            if cv.waitKey(10) & 0xFF == ord('q'):
+            cv.imshow('Frame', frame_cv)
+            if cv.waitKey(0) & 0xFF == ord('q'):
                 return -1
 
 
 def main():
     # Parameters
-    FRAMES = sorted(glob("./data/frames_1/*")) + sorted(glob("./data/frames_2/*")) + sorted(glob("./data/frames_3/*"))
+    # FRAMES = sorted(glob("./data/frames_1/*")) + sorted(glob("./data/frames_2/*")) + sorted(glob("./data/frames_3/*"))
+    FRAMES = sorted(glob("./data/sasha/*"))
 
     # Enable cudnn auto-tuner to find the best algorithm to use for your hardware
     torch.manual_seed(1)
